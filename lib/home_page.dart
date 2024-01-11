00ec8c23d13dcb44c865c8c42dfc1f5410d15f7f
soumi guria <soumiguria.cse2022@nsec.ac.in>
2024-01-11 21:10:51 +0530

    First commit

---
+++ lib/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+        borderRadius: BorderRadius.horizontal(left: Radius.circular(50)));
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          const Row(children: [
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text('Shoes\nCollection',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                  )),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                  border: border,
+                  enabledBorder: border,
+                ),
+              ),
+            ),
+          ]),
+          Expanded(
+            child: ListView.builder(itemBuilder: (context, index) {
+                itemCount:
+                filters.length;
+                itemBuilder:
+                (context, index) {
+                  final filter = filters[index];
+                  return Chip(label: Text(filter));
+                };
+              }),
+          )
+        ],
+      ),
+    ));
+  }
+}
